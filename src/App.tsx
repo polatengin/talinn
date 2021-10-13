341fd8dcc550e8342f170cb8511fc0aec109f592
polatengin <enpolat@microsoft.com>
2021-10-13 12:54:52 -0700

    adding webcam on the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,26 @@ import React, { useRef } from 'react';
 import Webcam from 'react-webcam';
 
 export const App: React.FunctionComponent = () => {
+
+  const webcam = useRef<Webcam>(null);
+
   return (
     <>
+    <header className="header">
+        <div className="title">face mask App</div>
+      </header>
+      <Webcam
+        audio={false}
+        ref={webcam}
+        style={{
+          position: "absolute",
+          margin: "auto",
+          textAlign: "center",
+          top: 100,
+          left: 0,
+          right: 0,
+        }}
+      />
     </>
   );
 }
-
-export default App;
